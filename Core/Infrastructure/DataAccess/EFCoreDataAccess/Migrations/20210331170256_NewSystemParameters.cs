959bb11877bec84041944e348cfd71eda1cbedf1
Dejan Boskovic <db@infogram.rs>
2021-03-31 19:18:18 +0200

    SystemParameters and admin added

---
+++ Core/Infrastructure/DataAccess/EFCoreDataAccess/Migrations/20210331170256_NewSystemParameters.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreDataAccess.Migrations
+{
+    public partial class NewSystemParameters : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+                                INSERT INTO SystemParameters(Name, Value)
+                                VALUES 
+                                    ('BonusDaysOnCreate', 7.00),
+                                    ('BonusTransfersPerMonth', 1.00);
+                                ";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Core/Infrastructure/DataAccess/EFCoreDataAccess/Migrations/20210331170603_AdminAccount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreDataAccess.Migrations
+{
+    public partial class AdminAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+                                INSERT INTO Accounts(Id, Bank, Balance, OpeningDate, LastTransactionDate, MonthlyIncome, MonthlyOutcome, Password, Blocked)
+                                VALUES 
+                                    ('0000000000000', 0, 0, '2000-01-01 00:00:00.000', '2000-01-01 00:00:00.000', 0, 0, 'adminX', 0);
+                                ";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
