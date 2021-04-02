e2ed7a203f2254a0042d4c30585e13c688d5d077
Dejan Boskovic <db@infogram.rs>
2021-04-02 12:51:08 +0200

    Implemented AccountManagement in frontend

---
+++ Applications/MoneyTransferWebApp/Pages/AccountManagement.cs
@@ -0,0 +1,96 @@
+﻿using ApplicationServices.Interfaces;
+using Blazored.Toast.Services;
+using Enums;
+using Microsoft.AspNetCore.Components;
+using MoneyTransferWebApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyTransferWebApp.Pages
+{
+    public partial class AccountManagement
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; private set; }
+        [Inject]
+        public IAdminService AdminService { get; private set; }
+        [Inject]
+        public IToastService ToastService { get; private set; }
+
+        public bool AdminLogged { get; private set; } = false;
+
+        public class StringWrapper
+        {
+            public string Data { get; set; }
+        }
+
+        public StringWrapper InputPassword { get; private set; } = new StringWrapper();
+        public StringWrapper InputIdForBlock { get; private set; } = new StringWrapper();
+        public StringWrapper InputIdForUnblock { get; private set; } = new StringWrapper();
+
+        protected async Task CheckAdmin()
+        {
+            try
+            {
+                var response = await AdminService.AdminCheck(InputPassword.Data);
+                if (response)
+                {
+                    ToastService.ShowSuccess("Dobrodošao admine", "Uspeh!");
+                    AdminLogged = true;
+                    StateHasChanged();
+                }
+                else
+                {
+                    ToastService.ShowError("Laž!", "Neuspeh!");
+                    AdminLogged = false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ToastService.ShowError(ex.Message, "Error!");
+                AdminLogged = false;
+            }
+            finally
+            {
+                InputPassword.Data = string.Empty;
+            }
+        }
+
+        protected async Task BlockAccount()
+        {
+            try
+            {
+                await AdminService.BlockAccount(InputIdForBlock.Data);
+                ToastService.ShowSuccess("I ako je bio blokiran i dalje je blokiran!", "Uspeh!");
+            }
+            catch (Exception ex)
+            {
+                ToastService.ShowError(ex.Message, "Error!");
+            }
+            finally
+            {
+                InputIdForBlock.Data = string.Empty;
+            }
+        }
+
+        protected async Task UnblockAccount()
+        {
+            try
+            {
+                await AdminService.UnblockAccount(InputIdForUnblock.Data);
+                ToastService.ShowSuccess("I ako je bio odblokiran i dalje je odblokiran!", "Uspeh!");
+            }
+            catch (Exception ex)
+            {
+                ToastService.ShowError(ex.Message, "Error!");
+            }
+            finally
+            {
+                InputIdForUnblock.Data = string.Empty;
+            }
+        }
+    }
+}
